644f32022bef8795e546967778c776a4a0237a87
efbe2121 <forjooxonly0@gmail.com>
2021-09-17 14:06:36 +0700

    initial

---
+++ vars/pipelineDeliv.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+//test
+
+def call(Map param){
+	pipeline {
+		agent {
+			label "dockerworker"
+		}
+		stages {
+			stage ("telegram notif"){
+				steps{
+					echo "${getMessage()} ${param.text}"
+				}
+			}
+			stage('Build') {
+				steps {
+					sh 'mvn -B -DskipTests clean package'
+				}
+			}
+			stage('Test') {
+				steps {
+					sh 'mvn test'
+				}
+				post {
+					always {
+						junit 'target/surefire-reports/*.xml'
+					}
+				}
+			}
+		}
+
+    }
+}
+
+def getMessage (){
+	def commiter = sh(script: "git show -s --pretty=%cn",returnStdout: true).trim()
+	def message = "$commiter deploying app"
+	return message
+}
